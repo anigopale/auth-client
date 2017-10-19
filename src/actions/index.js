c452b8c2eaefb83a7d1a9e91af49833b5d97eb51
Aniruddh Gopale <anigopale@gmail.com>
2017-10-19 13:38:18 +0530

    add token to local storage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,27 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = `http://localhost:3090`;
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
     // submit email/password to server
-    axios.post(`{$ROOT_URL}/signin`, { email, password });
+    axios.post(`{$ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // if request is good,
+        // - update state to indicate user is auth'ed
+        dispatch({ type: AUTH_USER });
+        // - save JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        //if request is bad..
+        // show an error to user
+      })
 
-    // if request is good,
-    // update state to indicate user is auth'ed
-    // save JWT token
-    // redirect to route /feature
 
-    //if request is bad..
-    // show an error to user
   }
 }
