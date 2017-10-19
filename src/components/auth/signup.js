5f1151bd159a083250425ab49d6d6891c18cb965
Aniruddh Gopale <anigopale@gmail.com>
2017-10-19 15:41:40 +0530

    create signup component, add route

---
+++ src/components/auth/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>Sign up</div>
+    )
+  }
+}
+export default Signup;
