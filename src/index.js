fc8ae3998d10f82f0c943a379f6eadab73696d3f
Aniruddh Gopale <anigopale@gmail.com>
2017-10-19 18:13:09 +0530

    change auth reducer using dispatch if token exists

--- src/index.js
+++ src/index.js
@@ -12,12 +12,21 @@ import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
 import Welcome from './components/welcome';
+import { AUTH_USER } from './actions/types';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if we have a token, consider the uesr to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App} >
         <IndexRoute component={Welcome} />
